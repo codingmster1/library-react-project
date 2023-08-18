3a3ce3def5afe66e5ac5ceb2c5770d9941b52125
Jeff <doylejeff98@gmail.com>
2023-08-18 12:30:27 -0400

    low to high filter

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Book from '../components/ui/Book';
 
 
-const Books = ({books}) => {
+const Books = ({books: initialBooks}) => {
+    const [books, setBooks] = useState(initialBooks);
+
+    function filterBooks(filter) {
+        if (filter === 'LOW_TO_HIGH') {
+            setBooks(books.slice().sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)))
+            
+        }
+
+    }
     return (
         <div id="books__body">
             <main id="books__main">
@@ -11,7 +20,7 @@ const Books = ({books}) => {
                         <div className="row">
                             <div className="books__header">
                                 <h2 className = "section__title books__header--title">All Books</h2>
-                                <select id ="filter" defaultValue="DEFAULT">
+                                <select id ="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event.target.value)}>
                                     <option value="DEFAULT"disabled>Sort</option>
                                     <option value="LOW_TO_HIGH">Price, Low to High</option>
                                     <option value="HIGH_TO_LOW">Price, High to Low</option>
