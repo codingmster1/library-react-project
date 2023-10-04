c9d70df0e9b84637aa9fb63f9aafb95acb60907b
Jeff <doylejeff98@gmail.com>
2023-10-04 02:11:18 -0400

    skeleton loading state fixed react strict mode del

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@
 import Footer from './components/Footer';
 import React, { useEffect, useState } from 'react';
 import Nav from './components/Nav';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Books from './pages/Books';
-import {books} from "./data";
+import { books } from "./data";
 import BookInfo from './pages/BookInfo';
 import Cart from './pages/Cart';
 
@@ -14,28 +14,27 @@ import Cart from './pages/Cart';
 function App() {
   const [cart, setCart] = useState([]);
 
-  function addToCart(book){
-    setCart([...cart, {...book, quantity: 1}])
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }])
   }
 
   function changeQuantity(book, quantity) {
     setCart(
       cart.map((item) => item.id === book.id
-      ?
+        ?
         {
-          ...item, 
+          ...item,
           quantity: +quantity,
 
         }
-      : item
-      
+        : item
+
+      )
     )
-  )
 
   }
 
-  function removeItem(item) 
-  {
+  function removeItem(item) {
     setCart(cart.filter(book => book.id !== item.id))
 
   }
@@ -48,23 +47,38 @@ function App() {
     return counter;
   }
 
-  
+
   useEffect(() => {
-console.log(cart)
+    console.log(cart)
   }, [cart])
 
 
   return (
     <Router>
-    <div className="App">
-    <Nav numberOfItems ={numberOfItems()}/>
-      <Route path="/" exact component={Home}/>
-      <Route path="/books" exact render={() => <Books books={books}/>}/>
-      <Route path="/books/:id" render={() => <BookInfo books={books} cart={cart} addToCart={addToCart} />} />
-      <Route path ="/cart" render={() => <Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem}/>} />
-      <Footer/>
-      </div>
-      </Router>
+      {/*  <div className="App"> */}
+      <Nav numberOfItems={numberOfItems()} />
+      <Route path="/" exact render={() => <Home />} />
+      <Route path="/books" exact render={() => <Books books={books} />} />
+      <Route
+        path="/books/:id"
+        render={() => (
+          <BookInfo books={books} cart={cart} addToCart={addToCart} />
+        )}
+      />
+      <Route
+        path="/cart"
+        render={() => (
+          <Cart
+            books={books}
+            cart={cart}
+            changeQuantity={changeQuantity}
+            removeItem={removeItem}
+          />
+        )}
+      />
+      <Footer />
+      {/*</div>*/}
+    </Router>
   );
 }
 
--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,10 +1,10 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef, useState } from 'react';
 import { Link } from "react-router-dom";
 import Price from "./Price";
 import Rating from "./Rating";
 
-const Book = ({book}) => {
+const Book = ({ book }) => {
     const [img, setImg] = useState();
 
     const mountedRef = useRef(true);
@@ -14,52 +14,52 @@ const Book = ({book}) => {
         image.src = book.url;
         image.onload = () => {
             setTimeout(() => {
-                if(mountedRef.current) {
-            setImg(image);
+                if (mountedRef.current) {
+                    setImg(image);
                 }
-        }, 300);
+            }, 300);
 
         };
         return () => {
-            mountedRef.current= true; // setting to false didn't operate correctly
+            mountedRef.current = false; // skeleton loading state fixed
         };
     })
-    
+
     return (
         <div className="book">
             {
                 img ? (
-                <>
-                <Link to={`/books/${book.id}`}>
-            <figure className="book__img--wrapper">
-                <img src={img.src}
-                alt="" 
-                className="book__img"
-                />
-            </figure>
-        </Link>
-        <div className="book__title">
-            <Link to={`/books/${book.id}`} className="book__title--link">
-                {book.title}
-            </Link>
+                    <>
+                        <Link to={`/books/${book.id}`}>
+                            <figure className="book__img--wrapper">
+                                <img src={img.src}
+                                    alt=""
+                                    className="book__img"
+                                />
+                            </figure>
+                        </Link>
+                        <div className="book__title">
+                            <Link to={`/books/${book.id}`} className="book__title--link">
+                                {book.title}
+                            </Link>
+                        </div>
+                        <Rating rating={book.rating} />
+                        <Price salePrice={book.salePrice} originalPrice={book.originalPrice}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <div className="book__img--skeleton"></div>
+                        <div className="skeleton book__title--skeleton"></div>
+                        <div className="skeleton book__rating--skeleton"></div>
+                        <div className="skeleton book__price--skeleton"></div>
+                    </>
+                )}
+
+
+
+
         </div>
-        <Rating rating ={book.rating} />
-       <Price salePrice={book.salePrice} originalPrice={book.originalPrice} 
-       />
-       </>
-       ) : ( 
-       <>
-       <div className="book__img--skeleton"></div>
-       <div className="skeleton book__title--skeleton"></div>
-       <div className="skeleton book__rating--skeleton"></div>
-       <div className="skeleton book__price--skeleton"></div> 
-       </>
-            )}
-            
-            
-        
-   
-    </div>
 
     );
 }
